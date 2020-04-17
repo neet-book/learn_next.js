18401b2070fedb850688fce69cc890d11e9210e3
yanoerika <yanoerika@163.com>
2020-04-17 16:56:29 +0800

    layout基本布局，city组件基本构建

---
+++ meituan/components/public/header/city.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="city">
+    <span class="el-icon-location"></span>
+    [{{ current }}]
+    <a class="change-city">切换城市</a>
+    [
+    <a
+      v-for="guess of nearby"
+      :key="guess.id"
+      :href="guess.link"
+      class="guess-city"
+      >
+      {{ guess.city }}
+    </a>
+    ]
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+interface city {
+  id: number
+  link: string
+  city: string
+}
+
+@Component
+export default class City extends Vue {
+  @Prop({ default: '' }) current!: string | undefined
+  @Prop({ default: [] }) nearby: city[] | undefined
+}
+</script>
+
+<style scoped>
+
+</style>
--- meituan/layouts/default.vue
+++ meituan/layouts/default.vue
@@ -1,55 +1,30 @@
 <template>
   <div>
-    <nuxt />
+    <header>
+      <city current="深圳" />
+    </header>
+    <main>
+      <nuxt/>
+    </main>
+    <footer>
+    </footer>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import City from '~/components/public/header/city.vue'
+@Component({
+  components: {
+    City
+  }
+})
+export default class  extends Vue {
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
 }
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+<style scoped>
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
