76b91c1f2ba8000c30f8843503f0158fcf72e4b2
yanoerika <yanoerika@163.com>
2020-04-24 21:37:13 +0800

    调整detial数据结构

--- pachong/src/db/index.js
+++ pachong/src/db/index.js
@@ -4,11 +4,14 @@ const url = 'mongodb://127.0.0.1:27017'
 
 class Database {
   constructor () {
-
+    this.client = null
+    this.db = null
+    this.collection = null
   }
 
-  async connect () {
-    return new Promise(res => {
+  // 链接数据库
+  async connect (database, collection) {
+    const client =  await new Promise(res => {
       MongoClient.connect(url, (err, client) => {
         if (err) {
           console.log('数据库连接失败： ', err)
@@ -18,7 +21,34 @@ class Database {
 
         res(client)
       })
+
+      this.client = client
+      if (database) this.db = client.db(database)
+      if (collecton) this.collection = client.db(database).collection(collection)
+      return client
     })
+
+    
   }
   
+  // 增
+  async insert(doc) {
+    return new Promise(res => {
+      if (!Array.isArray(doc)) doc = [doc]
+      this.collection.insertMany(doc, (err, result) => {
+        if (err) {
+          console.log(`数据添加失败：`, err)
+        } else {
+          console.log(`跟新数据成功`)
+        }
+        res()
+      })
+    })
+  }
 }
+
+const d = new Database()
+d.client('test', 'coll')
+.then(() => {
+
+})
