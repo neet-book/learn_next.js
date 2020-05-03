08c1e97ebf6a91f019c713c7dbbfaefdf0641752
yanoerika <yanoerika@163.com>
2020-05-03 21:14:35 +0800

    创建main组件

---
+++ meituan/components/public/main/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-body">
+    <div class="banner">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue} from 'nuxt-property-decorator'
+
+@Component
+export default class MainBody extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
