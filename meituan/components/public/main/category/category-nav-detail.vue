4e2d7fe967c249201f04549544de4aaf273d72c1
yanoerika <yanoerika@163.com>
2020-05-04 13:44:55 +0800

    详细分类的基础结构完成

---
+++ meituan/components/public/main/category/category-nav-detail.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="detail-container">
+    <div
+      class="detail-are"
+      v-for="cat of catDetails"
+      :key="cat.index"
+    >
+      <div class="detail-title-wrapper">
+        <h3>{{ cat.title }}</h3>
+        <i class="cl-icon-arrow-right">更多</i>
+      </div>
+      <div class="detail-content">
+        <nuxt-link
+          v-for="detail of cat.details"
+          :to="detail.href"
+          :key="detail.index"
+          target="_blank"
+        >
+          {{ detail.text }}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class CategoryNavDetail extends Vue {
+  @Prop(Array) catDetails: any[] | undefined
+  mounted() {
+    console.log(this.catDetails)
+  }
+}
+</script>
+
+<style scoped>
+.detail-container {
+  width: 400px;
+  background-color: #fff;
+}
+</style>
--- meituan/components/public/main/category/category-nav.vue
+++ meituan/components/public/main/category/category-nav.vue
@@ -1,43 +1,107 @@
 <template>
   <div class="category">
+    <h3 class="category-title">全部分类</h3>
     <div class="category-container">
-      <h3 class="category-nav-title">全部分类</h3>
       <ul>
-        <li class="category-li">
+        <li class="category-li"
+          v-for="category of categoryList"
+          :key="category.cat_id"
+          @mouseover="currentCat = category.cat_id"
+          @mouseleave="currentCat = false"
+        >
           <nuxt-link
-            v-for="(cat, index) of category"
+            v-for="(cat, index) of category.cats"
             :to="cat.link"
             :key="cat.index"
             target="_blank"
           >
             <!-- 如果索引加一比分类数量小则显示分隔符 -->
-            {{ category.length > index + 1  ?  cat.text + " / " : cat.text }}
+            {{ category.cats.length > index + 1  ?  cat.text + " /" : cat.text }}
           </nuxt-link>
           <i class="el-icon-arrow-right"></i>
         </li>
       </ul>
+      <category-nav-detail
+      v-for="cat of catDetails"
+      :key="cat.cat_id"
+      :catDetails="cat.details"
+      class="category-nav-detail"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue} from 'nuxt-property-decorator'
-
-@Component
+import CategoryNavDetail from './category-nav-detail.vue'
+@Component({
+  components: {
+    CategoryNavDetail
+  }
+})
 export default class CategoryNav extends Vue {
-  category = [
+  currentCat: number | boolean = 0
+
+  categoryList = [
     {
-    cat_id: 1,
-    index: '1.1',
-    text: '美食',
-    link: '#'
+      cat_id: 1,
+      cats:
+      [{
+        cat_id: 1,
+        index: '1.1',
+        text: '美食',
+        link: '#'
+      },
+      {
+        cat_id: 2,
+        index: '1.2',
+        text: '小吃',
+        link: '#'
+      }]
     },
     {
-    cat_id: 2,
-    index: '1.2',
-    text: '小吃',
-    link: '#'
-  }
+      cat_id: 2,
+      cats:
+      [{
+        cat_id: 2,
+        index: '2.1',
+        text: '美食',
+        link: '#'
+      },
+      {
+        cat_id: 2,
+        index: '2.2',
+        text: '小吃',
+        link: '#'
+      }]
+    }
+  ]
+
+  catDetails = [
+    {
+      cat_id: 1,
+      details: [
+        {
+          title: '机票',
+          cat_id: 1,
+          details: [
+            { 
+              "cat_id" : 1,
+              "detail_id" : 1,
+              "index" : "1_1_0",
+              "text" : "代金券",
+              "href" : "https://sz.meituan.com/meishi/c393/"
+            },
+            {
+              "cat_id" : 1,
+              "detail_id" : 1,
+              "index" : "1_1_1",
+              "text" : "甜点饮品",
+              "href" : "https://sz.meituan.com/meishi/c11/"
+            }
+          ]
+        }
+      ]
+    }
   ]
 }
 </script>
@@ -47,11 +111,15 @@ export default class CategoryNav extends Vue {
   width: 230px;
   background-color: #fff;
   border: 1px solid #e5e5e5;
+  padding-bottom: 8px;
 }
-
+.category-container {
+  position: relative;
+}
 .category-title {
   color: #666;
   font-size: 16px;
+  padding: 15px 15px 20px;
 }
 
 .category-li {
@@ -64,15 +132,24 @@ export default class CategoryNav extends Vue {
 
 .category-li > a {
   text-decoration: none;
-  color: var(--color-normal);
+  color: inherit;
 }
 
-/* .category-li > i {
+.category-li > i {
   position: absolute;
+  line-height: 24px;
   right: 12px;
-} */
+}
 
 .category-li:hover {
   background: rgba(255,150,0,0.08);
+  color: #222;
+}
+
+/* 详细分类 */
+.category-nav-detail {
+  position: absolute;
+  right: -100%;
+  top: 0;
 }
 </style>
