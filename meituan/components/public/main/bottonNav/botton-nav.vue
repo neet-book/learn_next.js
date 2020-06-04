e667dbc7160cd19f7632177a5c6ebf62e9370c5a
yanoerika <yanoerika@163.com>
2020-06-04 17:30:01 +0800

    完成botton-nav

--- meituan/components/public/main/bottonNav/botton-nav.vue
+++ meituan/components/public/main/bottonNav/botton-nav.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="botton-nav">
+    <h3 class="title">导航</h3>
     <nav-list
       v-for="n of navList"
       :key="n.c_id"
       :title="n.title"
       :items="n.items"
+      class="list"
     />
   </div>
 </template>
@@ -24,5 +26,26 @@ export default class BottonNav extends Vue {
 </script>
 
 <style scoped>
+.botton-nav {
+  margin: 40px 0;
+  background-color: white;
+  border: 1px solid #e5e5e5;
+}
 
+.title {
+  height: 44px;
+  line-height: 44px;
+  font-size: 16px;
+  color: #222;
+  font-weight: 500;
+  padding: 6px 0 0 20px;
+}
+
+.list >>> .b-n-items-container {
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.list:last-child >>> .b-n-items-container {
+  border-bottom: none;
+}
 </style>
--- meituan/components/public/main/bottonNav/nav-list.vue
+++ meituan/components/public/main/bottonNav/nav-list.vue
@@ -1,10 +1,11 @@
 <template>
-  <dl class="nav-list">
-    <dt>{{ title }}</dt>
-    <div class="nav-item-container">
+  <dl class="b-n-list clear-fix">
+    <dt class="b-n-list-title">{{ title }}</dt>
+    <div class="b-n-items-container clear-fix">
       <dd
         v-for="i of items"
         :key="i.c_id"
+        class="b-n-item"
       >
         <nuxt-link :to="i.href" target="_blank">{{ i.text }}</nuxt-link>
       </dd>
@@ -28,7 +29,43 @@ export default class NavList extends Vue {
 }
 </script>
 
+<style scope>
+.b-n-list {
+  height: 73px;
+}
 
-<style scoped>
+.b-n-list-title {
+  width: 100px;
+  padding-top: 10px;
+  float: left;
 
+  text-align: center;
+  color: #222;
+  font-size: 14px;
+  font-weight: 400;
+}
+
+.b-n-items-container {
+  width: 1088px;
+  height: auto;
+  overflow: hidden;
+  padding-top: 10px;
+  float: left;
+}
+
+.b-n-item {
+  float: left;
+  width: 100px;
+
+  font-size: 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  text-align: left;
+  margin-bottom: 13px;
+}
+.b-n-item a{
+  text-decoration: none;
+  color: #666;
+}
 </style>
